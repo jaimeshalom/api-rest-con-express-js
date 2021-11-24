88c1da51807b2a24389760b51a5545d7c6d18e2b
jaimeshalom <jaimeshalom12@gmail.com>
2021-11-24 11:27:32 -0400

    Commit to upload to Github

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const routerApi = require('./routes');
+
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const whitelist = ['http://127.0.0.1:5500', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  },
+};
+app.use(cors(options));
+
+app.get('/', (req, res) => {
+  res.send('Hola mi primer server en express');
+});
+
+routerApi(app);
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Mi port ${port} en http://localhost:${port}`);
+});
